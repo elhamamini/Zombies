6d5b4764f110c784f19aecd092bd72d8e5b6b11a
ABCK Corp <abckcorp.@ABCKs-iMac-2.local>
2020-02-21 13:44:04 -0500

    name change

--- app/components/root.js
+++ app/components/root.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Login from './LoginComponent';
+import Login from './Login';
 import Nav from './Nav';
 
 export default class Root extends Component {
