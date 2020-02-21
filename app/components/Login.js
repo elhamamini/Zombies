3278531e0a14c330aed89ad1a3107c6b6446cc4c
ABCK Corp <abckcorp.@ABCKs-iMac-2.local>
2020-02-21 13:47:27 -0500

    syntax

--- app/components/Login.js
+++ app/components/Login.js
@@ -9,7 +9,6 @@ import { Hr } from "./styled/Div";
 import { Form, FormRow, FormColumn } from "./styled/Form";
 import { Input, InputFeedback } from "./styled/Input";
 import Button from "./styled/Button";
-import { createGlobalStyle } from "styled-components";
 
 class Login extends Component {
   constructor() {
