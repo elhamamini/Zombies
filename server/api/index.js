d19bd089a452b422481cb7322e2fc7d3f56f68bd
Elham Amini <elhamfarvid@gmail.com>
2020-02-22 10:50:51 -0500

    still merge conflicts

--- server/api/index.js
+++ server/api/index.js
@@ -1,15 +1,12 @@
 const router = require('express').Router();
-<<<<<<< HEAD
 router.use('/github', require('./githubOuath'));
 router.use('/users', require('./users'));
-=======
 const conversationRouter = require('./conversation');
 const replyRouter = require('./reply');
 
 router.use('/conversation', conversationRouter);
 router.use('/reply', replyRouter);
 
->>>>>>> 1942569e8c6b593e0eb5f549d0a2fefc8badcc6c
 router.use('*', (req, res, next) => {
   const err = new Error('API route not found!');
   err.status = 404;
