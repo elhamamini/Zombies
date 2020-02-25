fcfbc26b4319f32f9317b220128fe562be2ff74f
Alexandra Morozoff <a.j.morozoff@gmail.com>
2020-02-24 23:48:38 -0500

    add tagging capability to create conversation page.

--- app/components/NewConversation.js
+++ app/components/NewConversation.js
@@ -7,8 +7,11 @@ import { Form } from './styled/Form';
 import { Input, TextField, InputFeedback, Label } from './styled/Input';
 import Button from './styled/Button';
 import { Select, Option } from './styled/Select';
+import { Pill, PillContainer, PillLabel } from './styled/Pill';
 import { getActiveUser } from '../redux/activeUser/thunks';
 import { getRepos } from '../redux/repository/thunks';
+import nlp from 'compromise';
+import whitelist from '../../whitelist';
 
 //TODO: Handle Successful Post by Redirecting to the Post
 class NewConversation extends Component {
@@ -21,22 +24,47 @@ class NewConversation extends Component {
         topicError: '',
         bodyError: '',
       },
+      tags: [],
     };
   }
+
   componentDidMount() {
     this.props.getActiveUser();
     setTimeout(() => {
       this.props.getRepos();
     }, 100);
   }
+
   handleOnChange = ({ target: { name, value } }) => {
-    this.setState({ [name]: value }, () => this.validate(name, value));
+    this.setState({ [name]: value }, () => {
+      this.validate(name, value);
+      //todo: call generateTags from here
+      this.generateTags(value);
+    });
   };
 
   handleOnClick = e => {
     e.preventDefault();
     this.props.postConversation(this.props.authentication.activeUser);
   };
+
+  generateTags = (value) => {
+    const { tags } = this.state;
+    const newTags = tags;
+    //run compromise on body content
+    let postTopics = nlp(value).normalize({ plurals:true, parentheses:true }).nouns();
+    //loop through returned terms   
+    console.log(postTopics.out('freq'));
+    postTopics.out('freq').forEach(term => {
+      //if term is NOT already in the tags list and IS in the whitelist, add it
+      if (!newTags.includes(term.reduced) && whitelist[term.reduced]) {
+          newTags.push(term.reduced);
+      }
+    });
+    if (newTags.length > tags.length) {
+      this.setState({ tags: newTags })
+    }
+  }
 
   validate = (name, value) => {
     const { errors } = this.state;
@@ -83,10 +111,10 @@ class NewConversation extends Component {
     const {
       topic,
       body,
+      tags,
       errors,
       errors: { topicError, bodyError },
     } = this.state;
-    console.log('active', this.props.activeUser);
     return (
       <MainContainer>
         <Form>
@@ -129,6 +157,13 @@ class NewConversation extends Component {
             onChange={this.handleOnChange}
           />
           <InputFeedback>{bodyError}</InputFeedback>
+          <PillContainer>
+            {
+              tags.length ?
+              tags.map(tag => <Pill key={tag}>{tag}</Pill>)
+              : ''
+            }
+          </PillContainer>
           <Button
             disabled={
               !topic || !body || Object.values(errors).some(val => !!val)
