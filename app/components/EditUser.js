20ad0934b14570d1c5127fabce8263a417234e17
Elham Amini <elhamfarvid@gmail.com>
2020-02-28 17:03:36 -0500

    pull new changes

---
+++ app/components/EditUser.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class EditProfile extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      email: '',
+      password: '',
+      image: '',
+      bio: '',
+    };
+  }
+}
--- app/redux/users/constants.js
+++ app/redux/users/constants.js
@@ -3,3 +3,4 @@ export const SET_ALL_USERS = Symbol('SET_ALL_USERS');
 export const SET_USER = Symbol('SET_USER');
 
 export const ADD_USER = Symbol('ADD_USER');
+export const EDIT_USER = Symbol('EDIT_USER');
