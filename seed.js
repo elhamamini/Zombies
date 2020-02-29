5889508ac748b3b47f07bef29d27d69842c7d265
Elham Amini <elhamfarvid@gmail.com>
2020-02-29 16:42:40 -0500

    seeding

--- seed.js
+++ seed.js
@@ -9,7 +9,7 @@ const {
 } = require('./server/db/index');
 const whitelist = require('./whitelist');
 const { conversationsList, replyList, usersList } = require('./seeding/index');
-
+const fullstackDB = require('./fullstackDB');
 const seed = async () => {
   try {
     await db.sync({ force: true });
@@ -25,17 +25,52 @@ const seed = async () => {
         })
       )
     );
-
-    const createdConversation = await Conversation.bulkCreate(
-      conversationsList
+    const titles = Object.keys(fullstackDB);
+    const createdConversation = await Promise.all(
+      titles.map(title => {
+        return Conversation.create({
+          title: title,
+          userId: Math.ceil(Math.random() * 7),
+        });
+      })
     );
 
+    console.log('cc', createdConversation);
+    const replies = await createdConversation.map(convObj => {
+      return Promise.all(
+        fullstackDB[convObj.title].map((reply, i) => {
+          console.log('reppy', reply);
+          console.log('convObj', convObj);
+          console.log('convObj', convObj.title);
+          if (i === 0) {
+            Reply.create({
+              body: reply,
+              userId: convObj.userId,
+              conversationId: convObj.id,
+            });
+          } else {
+            Reply.create({
+              body: reply,
+              userId: Math.ceil(Math.random() * 7),
+              conversationId: convObj.id,
+            });
+          }
+        })
+      );
+    });
+
+    // const createdConversation = await Conversation.bulkCreate(
+    //   conversationsList
+    // );
+
     for (let i = 0; i < createdConversation.length; i++) {
-      randomTag = createdTags[Math.round(Math.random() * 100) % createdTags.length];
+      console.log('here');
+      randomTag =
+        createdTags[Math.round(Math.random() * 100) % createdTags.length];
       await createdConversation[i].addTag(randomTag);
     }
 
-    await Reply.bulkCreate(replyList);
+    // await Reply.bulkCreate(replyList);
   } catch (err) {
     console.log(red(err));
   }
