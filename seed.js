9a0b4411048f0063d4b1b7daec920767eed50ed1
Elham Amini <elhamfarvid@gmail.com>
2020-03-01 10:00:06 -0500

    DB populated

--- seed.js
+++ seed.js
@@ -8,7 +8,7 @@ const {
   Tag,
 } = require('./server/db/index');
 const whitelist = require('./whitelist');
-const { conversationsList, replyList, usersList } = require('./seeding/index');
+const { usersList } = require('./seeding/index');
 const fullstackDB = require('./fullstackDB');
 const seed = async () => {
   try {
@@ -26,33 +26,37 @@ const seed = async () => {
       )
     );
     const titles = Object.keys(fullstackDB);
+
     const createdConversation = await Promise.all(
       titles.map(title => {
+        let answer = false;
+        if (fullstackDB[title].length > 1) {
+          answer = true;
+        }
         return Conversation.create({
           title: title,
           userId: Math.ceil(Math.random() * 7),
+          hasAnswer: answer,
+          views: Math.ceil(Math.random() * 100),
         });
       })
     );
-
-    console.log('cc', createdConversation);
     const replies = await createdConversation.map(convObj => {
       return Promise.all(
         fullstackDB[convObj.title].map((reply, i) => {
-          console.log('reppy', reply);
-          console.log('convObj', convObj);
-          console.log('convObj', convObj.title);
           if (i === 0) {
             Reply.create({
               body: reply,
               userId: convObj.userId,
               conversationId: convObj.id,
+              postNumber: i + 1,
             });
           } else {
             Reply.create({
               body: reply,
               userId: Math.ceil(Math.random() * 7),
               conversationId: convObj.id,
+              postNumber: i + 1,
             });
           }
         })
@@ -64,13 +68,10 @@ const seed = async () => {
     // );
 
     for (let i = 0; i < createdConversation.length; i++) {
-      console.log('here');
       randomTag =
         createdTags[Math.round(Math.random() * 100) % createdTags.length];
       await createdConversation[i].addTag(randomTag);
     }
-
-    // await Reply.bulkCreate(replyList);
   } catch (err) {
     console.log(red(err));
   }
