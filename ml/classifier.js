f86f3920acf103ff014f0ad2133293ea240f78ce
Alexandra Morozoff <a.j.morozoff@gmail.com>
2020-03-04 20:05:01 -0500

    update

--- ml/classifier.js
+++ ml/classifier.js
@@ -4,21 +4,10 @@ const chalk = require('chalk');
 
 const fs = require('fs');
 
-let rawdata = fs.readFileSync('classifiedSet.json');
-let classifiedSet = JSON.parse(rawdata);
 
-
-const net = new brain.recurrent.LSTM();
-testNet.train(classifiedSet, { 
-    iterations: 50,
-    log: (status) => {
-        console.log(status);
-    },
-})
-
-
-
-const jsonNet = net.toJSON();
-fs.writeFileSync('trainedNet.json', jsonNet, (e) => {
-    if (e) console.log(e)
-});
+//TODO: load net in from json, test some inputs
+const rawNet = fs.readFileSync('trainedNet.json');
+let net = JSON.parse(rawNet);
+// const net = new brain.LSTMTimeStep()
+// net.fromJSON(json)
+// net.run(input)
