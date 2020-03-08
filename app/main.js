90ed3ecb657d9f6406845547a16e17b7f4544252
Elham Amini <elhamfarvid@gmail.com>
2020-03-08 12:58:34 -0400

    root

--- app/main.js
+++ app/main.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
-import Root from './components/Root';
+import Root from './components/root';
 
 render(
   <Provider store={store}>
